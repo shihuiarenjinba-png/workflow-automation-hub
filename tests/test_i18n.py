739f5128c4f3256726294415043831aca1ebaa90
shihuiarenjinba-png <shihuiarenjinba@gmail.com>
2026-09-16 18:14:47 +0900

    Lock bilingual language selector label in i18n tests

--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -8,6 +8,10 @@ class I18nTests(unittest.TestCase):
         self.assertNotEqual(tr('ja', 'tab_jobs'), tr('en', 'tab_jobs'))
         self.assertIn('Workflow', tr('en', 'app_title'))
 
+    def test_language_selector_label_is_always_bilingual(self):
+        self.assertEqual(tr('ja', 'language'), '言語 / Language')
+        self.assertEqual(tr('en', 'language'), '言語 / Language')
+
 
 if __name__ == '__main__':
     unittest.main()
